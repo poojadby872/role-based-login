d2060d886dd9d2040fdf36f47276fe4fbf09ce76
pooja <poojadby872@gmail.com>
2024-10-21 21:39:12 +0530

    enabled switching the roles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const { role, setRole } = useContext(RoleContext);
-  const [disable,setDisable] = useState(false)
+  // const [disable,setDisable] = useState(false)
 
   
 
@@ -24,13 +24,13 @@ const Navbar = () => {
     <h2>Role Based Access Control</h2>
     <div className='Buttons'  >
     <Link className='navLink' to='/users/admin'>
-     <button disabled={disable} id="btn" className={role === 'Admin' ? 'active' : 'inactive'}  value="Admin"  onChange={handleRoleChange} onClick={()=>{setRole("Admin"); setDisable(true)}} >Admin</button>
+     <button  id="btn" className={role === 'Admin' ? 'active' : 'inactive'}  value="Admin"  onChange={handleRoleChange} onClick={()=>setRole("Admin")} >Admin</button>
     </Link>
     <Link className='navLink' to='/users/editor'>
-      <button disabled={disable}  id="btn" className={role === 'Editor' ? 'active' :'inactive'} value="Editor"   onChange={handleRoleChange} onClick={()=>{setRole("Editor"); setDisable(true) }}>Editor </button>
+      <button  id="btn" className={role === 'Editor' ? 'active' :'inactive'} value="Editor"   onChange={handleRoleChange} onClick={()=>setRole("Editor")}>Editor </button>
     </Link>
     <Link className='navLink' to='/viewer'>
-       <button disabled={disable} id="btn" className={role === 'Viewer' ? 'active' :'inactive'} value="Viewer"   onChange={handleRoleChange} onClick={()=>{setRole("Viewer");setDisable(true) }}>Viewer</button>
+       <button  id="btn" className={role === 'Viewer' ? 'active' :'inactive'} value="Viewer"   onChange={handleRoleChange} onClick={()=>setRole("Viewer")}>Viewer</button>
     </Link>
      
     </div>
